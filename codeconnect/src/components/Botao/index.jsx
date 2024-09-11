5a853c2623068b46acee343dde04526f50901148
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-09-11 12:29:31 -0300

    feat button component

---
+++ codeconnect/src/components/Botao/index.jsx
@@ -0,0 +1,14 @@
+import "./Botao.css";
+
+function Botao() {
+    return (
+        <button 
+            type="submit"
+            className="form__botao"
+        >
+            Login
+        </button>
+    )
+}
+
+export default Botao;
