4b58781f4fbfc99a60f21d6fcb951661bb57aa43
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-09-11 12:30:22 -0300

    feat texting field component

--- codeconnect/src/components/CampoDeDigitacao/index.jsx
+++ codeconnect/src/components/CampoDeDigitacao/index.jsx
@@ -1,5 +1,17 @@
-function CampoDeDigitacao() {
-    return ();
+import "./CampoDeDigitacao.css";
+
+function CampoDeDigitacao({label, tipo, placeholder}) {
+    return (
+        <div className='form__campo-digitacao' >
+            <label for='email'>{label}</label>
+            <input 
+                type={tipo}
+                placeholder={placeholder}
+                required
+                id='email'
+            />
+        </div>
+    );
 };
 
 export default CampoDeDigitacao;
